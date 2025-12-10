c613f6bfde61106d0d49deaab9d003ab7209f65c
Dzkzzz <dzdzaakk@gmail.com>
2025-12-10 21:06:57 +0700

    vue app lifecycle

--- section-4/app.js
+++ section-4/app.js
@@ -16,10 +16,37 @@ const app = Vue.createApp({
 
     },
   },
+  beforeCreate() {
+    console.log('beforeCreate()');
+  },
+  created() {
+    console.log('created()');
+  },
+  beforeMount() {
+    console.log('beforeMount()');
+  },
+  mounted() {
+    console.log('mounted()');
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate()');
+  },
+  updated() {
+    console.log('updated()');
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount()');
+  },
+  unmounted() {
+    console.log('unmounted()');
+  },
 });
 
 app.mount('#app');
 
+setTimeout(function () {
+  app.unmount();
+}, 3000);
 
 const app2 = Vue.createApp({
   template: `
