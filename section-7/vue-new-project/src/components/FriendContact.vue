64f0b001ab0a1228fa0d4a9391313aafa395f8b7
Dzkzzz <dzdzaakk@gmail.com>
2025-12-19 20:28:09 +0700

    initial setup

---
+++ section-7/vue-new-project/src/components/FriendContact.vue
@@ -0,0 +1,37 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails(friend.id)">{{ detailsAreVisible ? 'Hide' : 'Show'}} Details</button>
+        <ul v-if="detailsAreVisible">
+            <li>
+                <strong>Phone:</strong>
+                {{ friend.phone }}
+            </li>
+            <li>
+                <strong>Email:</strong>
+                {{ friend.email }}
+            </li>
+        </ul>
+    </li>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                detailsAreVisible: false,
+                friend: {
+                    id: 'manuel',
+                    name:'Manuel Lorenz',
+                    phone: '0123 4567 890',
+                    email: 'manuel@localhost.com'
+                },
+            }
+        },
+        methods:{
+            toggleDetails(){
+                this.detailsAreVisible = !this.detailsAreVisible;
+            }
+        }
+    };
+</script>
